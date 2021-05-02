766a90913b4133ca1198b22a7eb7646587345e69
DESKTOP-M0IVT81\Test <apprentice.soft@gmail.com>
2021-05-02 18:59:08 -0400

    Initial commit

---
+++ android/src/com/apprenticesoft/brickbreaker2/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.apprenticesoft.brickbreaker2;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.apprenticesoft.brickbreaker2.BrickBreaker2;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BrickBreaker2(), config);
+	}
+}
---
+++ desktop/src/com/apprenticesoft/brickbreaker2/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.apprenticesoft.brickbreaker2.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.apprenticesoft.brickbreaker2.BrickBreaker2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BrickBreaker2(), config);
+	}
+}
